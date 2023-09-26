32862164395649b5ad54b358a7db6840050b27e2
iskrin <91634355+iskrin@users.noreply.github.com>
2023-09-26 18:00:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,305 @@
+let stolice = [
+  { państwo: "Afganistan", stolica: "Kabul" },
+  { państwo: "Albania", stolica: "Tirana" },
+  { państwo: "Algieria", stolica: "Algier" },
+  { państwo: "Andora", stolica: "Andora" },
+  { państwo: "Angola", stolica: "Luanda" },
+  { państwo: "Antigua i Barbuda", stolica: "Saint John's" },
+  { państwo: "Arabia Saudyjska", stolica: "Rijad" },
+  { państwo: "Argentyna", stolica: "Buenos Aires" },
+  { państwo: "Armenia", stolica: "Erywań" },
+  { państwo: "Australia", stolica: "Canberra" },
+  { państwo: "Austria", stolica: "Wiedeń" },
+  { państwo: "Azerbejdżan", stolica: "Baku" },
+  { państwo: "Bahamy", stolica: "Nassau" },
+  { państwo: "Bahrajn", stolica: "Manama" },
+  { państwo: "Bangladesz", stolica: "Dhaka" },
+  { państwo: "Barbados", stolica: "Bridgetown" },
+  { państwo: "Belgia", stolica: "Bruksela" },
+  { państwo: "Belize", stolica: "Belmopan" },
+  { państwo: "Benin", stolica: "Porto-Novo" },
+  { państwo: "Bhutan", stolica: "Thimphu" },
+  { państwo: "Białoruś", stolica: "Mińsk" },
+  { państwo: "Boliwia", stolica: "La Paz" },
+  { państwo: "Bośnia i Hercegowina", stolica: "Sarajewo" },
+  { państwo: "Botswana", stolica: "Gaborone" },
+  { państwo: "Brazylia", stolica: "Brasilia" },
+  { państwo: "Brunei", stolica: "Bandar Seri Begawan" },
+  { państwo: "Bułgaria", stolica: "Sofia" },
+  { państwo: "Burkina Faso", stolica: "Wagadugu" },
+  { państwo: "Burundi", stolica: "Gitega" },
+  { państwo: "Chile", stolica: "Santiago" },
+  { państwo: "Chiny", stolica: "Pekin" },
+  { państwo: "Chorwacja", stolica: "Zagrzeb" },
+  { państwo: "Cypr", stolica: "Nikozja" },
+  { państwo: "Czad", stolica: "Ndżamena" },
+  { państwo: "Czarnogóra", stolica: "Podgorica" },
+  { państwo: "Czechy", stolica: "Praga" },
+  { państwo: "Dania", stolica: "Kopenhaga" },
+  { państwo: "DRK", stolica: "Kinszasa" },
+  { państwo: "Dominika", stolica: "Roseau" },
+  { państwo: "Dominikana", stolica: "Santo Domingo" },
+  { państwo: "Dżibuti", stolica: "Dżibuti" },
+  { państwo: "Egipt", stolica: "Kair" },
+  { państwo: "Ekwador", stolica: "Quito" },
+  { państwo: "Erytrea", stolica: "Asmara" },
+  { państwo: "Estonia", stolica: "Talinn" },
+  { państwo: "Eswatini", stolica: "Mbabane" },
+  { państwo: "Etiopia", stolica: "Addi s Abeba" },
+  { państwo: "Fidżi", stolica: "Suva" },
+  { państwo: "Filipiny", stolica: "Manila" },
+  { państwo: "Finlandia", stolica: "Helsinki" },
+  { państwo: "Francja", stolica: "Paryż" },
+  { państwo: "Gabon", stolica: "Libreville" },
+  { państwo: "Gambia", stolica: "Bandżul" },
+  { państwo: "Ghana", stolica: "Akra" },
+  { państwo: "Grecja", stolica: "Ateny" },
+  { państwo: "Grenada", stolica: "Saint George's" },
+  { państwo: "Gruzja", stolica: "Tbilisi" },
+  { państwo: "Gujana", stolica: "Georgetown" },
+  { państwo: "Gwatemala", stolica: "Gwatemala" },
+  { państwo: "Gwinea", stolica: "Konakry" },
+  { państwo: "Gwinea Bissau", stolica: "Bissau" },
+  { państwo: "Gwinea Równikowa", stolica: "Malabo" },
+  { państwo: "Haiti", stolica: "Port-au-Prince" },
+  { państwo: "Hiszpania", stolica: "Madryt" },
+  { państwo: "Holandia", stolica: "Amsterdam" },
+  { państwo: "Honduras", stolica: "Tegucigalpa" },
+  { państwo: "Indie", stolica: "Nowe Delhi" },
+  { państwo: "Indonezja", stolica: "Dżakarta" },
+  { państwo: "Irak", stolica: "Bagdad" },
+  { państwo: "Iran", stolica: "Teheran" },
+  { państwo: "Irlandia", stolica: "Dublin" },
+  { państwo: "Islandia", stolica: "Reykjavik" },
+  { państwo: "Izrael", stolica: "Jerozolima" },
+  { państwo: "Jamajka", stolica: "Kingston" },
+  { państwo: "Japonia", stolica: "Tokio" },
+  { państwo: "Jemen", stolica: "Sana" },
+  { państwo: "Jordania", stolica: "Amman" },
+  { państwo: "Kambodża", stolica: "Phnom Penh" },
+  { państwo: "Kamerun", stolica: "Juande" },
+  { państwo: "Kanada", stolica: "Ottawa" },
+  { państwo: "Katar", stolica: "Doha" },
+  { państwo: "Kazachstan", stolica: "Nur-Sułtan" },
+  { państwo: "Kenia", stolica: "Nairobi" },
+  { państwo: "Kirgistan", stolica: "Biszkek" },
+  { państwo: "Kiribati", stolica: "Tarawa" },
+  { państwo: "Kolumbia", stolica: "Bogota" },
+  { państwo: "Komory", stolica: "Moroni" },
+  { państwo: "Kongo", stolica: "Brazzaville" },
+  { państwo: "Korea Płd", stolica: "Seul" },
+  { państwo: "Korea Płn", stolica: "Pjongjang" },
+  { państwo: "Kosowo", stolica: "Prisztina" },
+  { państwo: "Kostaryka", stolica: "San Jose" },
+  { państwo: "Kuba", stolica: "Hawana" },
+  { państwo: "Kuwejt", stolica: "Kuwejt" },
+  { państwo: "Laos", stolica: "Wientian" },
+  { państwo: "Lesotho", stolica: "Maseru" },
+  { państwo: "Liban", stolica: "Bejrut" },
+  { państwo: "Liberia", stolica: "Monrovia" },
+  { państwo: "Libia", stolica: "Trypolis" },
+  { państwo: "Liechtenstein", stolica: "Vaduz" },
+  { państwo: "Litwa", stolica: "Wilno" },
+  { państwo: "Luksemburg", stolica: "Luksemburg" },
+  { państwo: "Łotwa", stolica: "Ryga" },
+  { państwo: "Macedonia Płn", stolica: "Skopje" },
+  { państwo: "Madagaskar", stolica: "Antananarywa" },
+  { państwo: "Malawi", stolica: "Lilongwe" },
+  { państwo: "Malediwy", stolica: "Male" },
+  { państwo: "Malezja", stolica: "Kuala Lumpur" },
+  { państwo: "Mali", stolica: "Bamako" },
+  { państwo: "Malta", stolica: "Valetta" },
+  { państwo: "Maroko", stolica: "Rabat" },
+  { państwo: "Mauretania", stolica: "Nawakszut" },
+  { państwo: "Mauritius", stolica: "Port Louis" },
+  { państwo: "Meksyk", stolica: "Meksyk" },
+  { państwo: "Mikronezja", stolica: "Palikir" },
+  { państwo: "Mjanma", stolica: "Naypyidaw" },
+  { państwo: "Mołdawia", stolica: "Kiszyniów" },
+  { państwo: "Monako", stolica: "Monako" },
+  { państwo: "Mongolia", stolica: "Ułan Bator" },
+  { państwo: "Mozambik", stolica: "Maputo" },
+  { państwo: "Namibia", stolica: "Windhuk" },
+  { państwo: "Nauru", stolica: "Yaren" },
+  { państwo: "Nepal", stolica: "Katmandu" },
+  { państwo: "Niemcy", stolica: "Berlin" },
+  { państwo: "Niger", stolica: "Niamey" },
+  { państwo: "Nigeria", stolica: "Abudża" },
+  { państwo: "Nikaragua", stolica: "Managua" },
+  { państwo: "Norwegia", stolica: "Oslo" },
+  { państwo: "Nowa Zelandia", stolica: "Wellington" },
+  { państwo: "Oman", stolica: "Maskat" },
+  { państwo: "Pakistan", stolica: "Islambad" },
+  { państwo: "Palau", stolica: "Ngerulmud" },
+  { państwo: "Panama", stolica: "Panama" },
+  { państwo: "Papua Nowa Gwinea", stolica: "Port Moresby" },
+  { państwo: "Paragwaj", stolica: "Asuncion" },
+  { państwo: "Peru", stolica: "Lima" },
+  { państwo: "Polska", stolica: "Warszawa" },
+  { państwo: "RPA", stolica: "Pretoria" },
+  { państwo: "Portugalia", stolica: "Lizbona" },
+  { państwo: "RŚA", stolica: "Bangi" },
+  { państwo: "Republika Zielonego Przylądka", stolica: "Praia" },
+  { państwo: "Rosja", stolica: "Moskwa" },
+  { państwo: "Rumunia", stolica: "Bukareszt" },
+  { państwo: "Rwanda", stolica: "Kigali" },
+  { państwo: "Saint Kitts i Nevis", stolica: "Basseterre" },
+  { państwo: "Saint Lucia", stolica: "Castries" },
+  { państwo: "Saint Vincent i Grenadyny", stolica: "Kingstown" },
+  { państwo: "Salwador", stolica: "San Salvador" },
+  { państwo: "Samoa", stolica: "Apia" },
+  { państwo: "San Marino", stolica: "San Marino" },
+  { państwo: "Senegal", stolica: "Dakar" },
+  { państwo: "Serbia", stolica: "Belgrad" },
+  { państwo: "Seszele", stolica: "Victoria" },
+  { państwo: "Sierra Leone", stolica: "Freetown" },
+  { państwo: "Singapur", stolica: "Singapur" },
+  { państwo: "Słowacja", stolica: "Bratysława" },
+  { państwo: "Słowenia", stolica: "Lublana" },
+  { państwo: "Somalia", stolica: "Mogadiszu" },
+  { państwo: "Sri Lanka", stolica: "Kotte" },
+  { państwo: "USA", stolica: "Waszyngton" },
+  { państwo: "Sudan", stolica: "Chartum" },
+  { państwo: "Sudan Płd", stolica: "Dżuba" },
+  { państwo: "Surinam", stolica: "Paramaribo" },
+  { państwo: "Syria", stolica: "Damaszek" },
+  { państwo: "Szwajcaria", stolica: "Berno" },
+  { państwo: "Szwecja", stolica: "Sztokholm" },
+  { państwo: "Tadżykistan", stolica: "Duszanbe" },
+  { państwo: "Tajlandia", stolica: "Bangkok" },
+  { państwo: "Tajwan", stolica: "Tajpej" },
+  { państwo: "Tanzania", stolica: "Dodoma" },
+  { państwo: "Timor Wschodni", stolica: "Dili" },
+  { państwo: "Togo", stolica: "Lome" },
+  { państwo: "Tonga", stolica: "Nuku'alofa" },
+  { państwo: "Trynidad i Tobago", stolica: "Port-of-Spain" },
+  { państwo: "Tunezja", stolica: "Tunis" },
+  { państwo: "Turcja", stolica: "Ankara" },
+  { państwo: "Turkmenistan", stolica: "Aszchabad" },
+  { państwo: "Tuvalu", stolica: "Funafuti" },
+  { państwo: "Uganda", stolica: "Kampala" },
+  { państwo: "Ukraina", stolica: "Kijów" },
+  { państwo: "Urugwaj", stolica: "Montevideo" },
+  { państwo: "Uzbekistan", stolica: "Taszkent" },
+  { państwo: "Vanuatu", stolica: "Port Vila" },
+  { państwo: "Watykan", stolica: "Watykan" },
+  { państwo: "Wenezuela", stolica: "Caracas" },
+  { państwo: "Węgry", stolica: "Budapeszt" },
+  { państwo: "Wielka Brytania", stolica: "Londyn" },
+  { państwo: "Wietnam", stolica: "Hanoi" },
+  { państwo: "Włochy", stolica: "Rzym" },
+  { państwo: "WKS", stolica: "Jamusukro" },
+  { państwo: "Wyspy Marshalla", stolica: "Majuro" },
+  { państwo: "Wyspy Salomona", stolica: "Honiara" },
+  { państwo: "Wyspy Świętego Tomasza i Książęca", stolica: "Sao Tome" },
+  { państwo: "Zambia", stolica: "Lusaka" },
+  { państwo: "Zimbabwe", stolica: "Harare" },
+  { państwo: "ZEA", stolica: "Abu Zabi" },
+];
+
+let tbody1 = document.getElementById("tableBody1");
+let tbody2 = document.getElementById("tableBody2");
+let tbody3 = document.getElementById("tableBody3");
+
+let answerInput = document.getElementById("answerInput");
+
+let giveUpButton = document.querySelector(".giveUpButton");
+
+let refreshButton = document.querySelector(".refreshButton");
+
+const minNumber = 0;
+const maxNumber = stolice.length - 1;
+
+//tworzy tablice z losową sekwęcją liczb od 0 do stolice.length
+function getRandomSequence(min, max) {
+  const sequence = [];
+
+  // Create an array with numbers from min to max
+  for (let i = min; i <= max; i++) {
+    sequence.push(i);
+  }
+
+  // Shuffle the array using the Fisher-Yates algorithm
+  for (let i = sequence.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [sequence[i], sequence[j]] = [sequence[j], sequence[i]];
+  }
+
+  return sequence;
+}
+
+const randomSequence = getRandomSequence(minNumber, maxNumber);
+
+//Gneruje pierwszą tabelę
+for (i = 0; i < 65; i++) {
+  tbody1.innerHTML += `
+    <tr>
+        <td>${stolice[randomSequence[i]].państwo}</td>
+        <td id = "${randomSequence[i]}", class = "hidden">${
+    stolice[randomSequence[i]].stolica
+  }</td>
+    </tr>`;
+}
+
+//Gneruje drugą tabelę
+for (i = 65; i < 130; i++) {
+  tbody2.innerHTML += `
+      <tr>
+          <td>${stolice[randomSequence[i]].państwo}</td>
+          <td id = "${randomSequence[i]}", class = "hidden">${
+    stolice[randomSequence[i]].stolica
+  }</td>
+      </tr>`;
+}
+
+//Gneruje trzecią tabelę
+for (i = 130; i < stolice.length; i++) {
+  tbody3.innerHTML += `
+        <tr>
+            <td>${stolice[randomSequence[i]].państwo}</td>
+            <td id = "${randomSequence[i]}", class = "hidden">${
+    stolice[randomSequence[i]].stolica
+  }</td>
+        </tr>`;
+}
+
+//wykonuje sę przy evencie oninput
+function checkAnswer() {
+  for (i = 0; i < stolice.length; i++) {
+    if (
+      decomposeString(answerInput.value) == decomposeString(stolice[i].stolica)
+    ) {
+      let cellToChange = document.getElementById(String(i));
+      if (!cellToChange.classList.contains("answered")) {
+        cellToChange.classList.add("answered");
+        showCell(i);
+        answerInput.value = "";
+      }
+    }
+  }
+}
+
+giveUpButton.addEventListener("click", () => {
+  for (i = 0; i < stolice.length; i++) {
+    showCell(i);
+  }
+});
+
+refreshButton.addEventListener("click", () => {
+  location.reload();
+});
+
+//usuwa wszystkie dziwne znaczki, spacje i przekształca do małych liter
+function decomposeString(inputString) {
+  return inputString.replace(/[^a-zA-Z]/g, "").toLowerCase();
+}
+function showCell(i) {
+  let cellToChange = document.getElementById(String(i));
+  cellToChange.classList.remove("hidden");
+
+  if (cellToChange.classList.contains("answered")) {
+    cellToChange.classList.add("correct");
+  } else {
+    cellToChange.classList.add("wrong");
+  }
+}
